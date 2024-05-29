739ffb38fcd074b1ce7c6bd84be859cf8462b46d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-30 00:16:53 +0100

    Updated index.js, package-lock.json, and package.json files with changes to server creation, template file reading, and dependencies.

--- Basics/index.js
+++ Basics/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { dirname } = require('path');
 const url = require('url');
 const replaceTemplate = require('./modules/replaceTemplate.js');
+const slugify = require('slugify');
 /* ------------------------ FILES ----------------------- */
 
 // // reading a text file, blocking synchronous way
@@ -36,19 +37,13 @@ const replaceTemplate = require('./modules/replaceTemplate.js');
 
 /* ----------------------- SERVER ----------------------- */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -58,9 +53,7 @@ const server = http.createServer((req, res) => {
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join('');
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
@@ -84,6 +77,4 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Page not found</h1>');
   }
 });
-server.listen(8000, '127.0.0.1', () =>
-  console.log('Listening to requests on port 8000.')
-);
+server.listen(8000, '127.0.0.1', () => console.log('Listening to requests on port 8000.'));
