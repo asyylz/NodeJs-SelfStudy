6014cbbe94cf4079f55d68c0e49b449fabde23fd
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-29 21:12:49 +0100

    simple API

--- Basics/index.js
+++ Basics/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const { dirname } = require('path');
 const url = require('url');
 /* ------------------------ FILES ----------------------- */
 
@@ -33,13 +34,18 @@ const url = require('url');
 // });
 
 /* ----------------------- SERVER ----------------------- */
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-
   if (pathName === '/' || pathName === '/overview') {
     res.end('This is the overview');
   } else if (pathName === '/product') {
     res.end('This is the product');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'content-type': 'text/html',
