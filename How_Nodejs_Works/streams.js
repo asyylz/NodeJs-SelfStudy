5abbf5d4d296c0e9cfd470cb703e4837548646ca
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-30 21:43:25 +0100

    streams pipe

---
+++ How_Nodejs_Works/streams.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  //Solution 1:
+  // fs.readFile('test-file.txt', (err, data) => {
+  //   res.end(data);
+  // });
+
+  //Solution2: problem is backpressure
+  //   const readable = fs.createReadStream('test-file.txt');
+  //   readable.on('data', (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on('end', () => {
+  //     res.end();
+  //   });
+  //   readable.on('error', (err) => {
+  //     console.log(err);
+  //     res.statusCode === 500;
+  //     res.end('File not found!');
+  //   });
+
+  //Solution3:
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res);
+  // readableSource.pipe(writeableDestination)
+});
+
+server.listen(8000, '127.0.01', () => {
+  console.log('Listening...');
+});
