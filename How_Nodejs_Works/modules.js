b4fd1a73cb98a1d3e4c76d8cb307cb9c20b9066b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-31 13:18:24 +0100

    Added console log statement to print built-in modules, and added an empty line to the code."

--- How_Nodejs_Works/modules.js
+++ How_Nodejs_Works/modules.js
@@ -1,5 +1,6 @@
 // console.log(arguments);
 // console.log(require('module').wrapper);
+console.log(require('module').builtinModules)
 
 const C = require('./test-module-1');
 const calc1 = new C();
@@ -10,6 +11,7 @@ console.log(calc1.add(1, 6));
 //const calc2 = require('./test-module-2');
 const { add, multiply, devide } = require('./test-module-2');
 console.log(multiply(2, 5));
+
 
 //caching
 require('./test-module-3')();
