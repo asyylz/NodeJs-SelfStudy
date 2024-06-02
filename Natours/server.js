dbbbbec631221f719b28d201754f90a628efac42
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-02 17:56:07 +0100

    Added mongoose dependency and configured MongoDB connection, updated package.json and server.js files

--- Natours/server.js
+++ Natours/server.js
@@ -1,9 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env.local' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    //.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true
+  })
+  .then(con => {
+    console.log(con.connections);
+    console.log('asiye');
+  });
 
-console.log(process.env.NODE_ENV);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
