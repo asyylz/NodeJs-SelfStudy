2ca32d33de559f3baacbaaa84b61ef992db2d69d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-02 23:54:57 +0100

    Refactored tourController.js to improve query building and added pagination, sorting, and field limiting. Updated tourModel.js to set select:false for createdAt field.

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -5,31 +5,57 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
+
     // Build Query
-    // 1)Filtering
+    // 1a)Filtering
     const queryObject = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObject[el]);
     console.log(req.query, queryObject);
 
-    // 2) Advanced filtering
+    // 1b) Advanced filtering
     let queryStr = JSON.stringify(queryObject);
     // g flag replace all accourance
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
     //console.log(JSON.parse(queryStr));
 
     // writing query 1:
-    const query = Tour.find(JSON.parse(queryStr));
-
-    // Execute Query
-    const tours = await query;
-
+    let query = Tour.find(JSON.parse(queryStr));
     // writing query 2:
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field Limitting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4)Pagination, 1-10 page 1, 11-20 page 2
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // Execute Query
+    const tours = await query;
 
     // Send Response
     res.status(200).json({
