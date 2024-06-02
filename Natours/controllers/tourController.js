08687cae11de136d1cb9ee9862414b6e8fc819fa
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-03 00:06:57 +0100

    Added aliasTopTours function to tourController and new route /top-5-cheap to tourRoutes

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 // 1-) ROUTES HANDLERS
 // GET Tours:sending back to the client
 exports.getAllTours = async (req, res) => {
