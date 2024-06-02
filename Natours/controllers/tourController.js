ab880045a5e557cd4e7a4136e204fe72ec8c9313
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-02 18:45:11 +0100

    Removed file system operations and replaced with mongoose model; commented out code related to tour data and id checking

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,19 +1,15 @@
-const fs = require('fs');
-// this line on the top of the file and will be read once so no blocking
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('./../models/tourModel');
 
-exports.checkId = (req, res, next, val) => {
-  console.log(`Tour id is:${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
+// exports.checkId = (req, res, next, val) => {
+//   console.log(`Tour id is:${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID'
+//     });
+//   }
+//   next();
+// };
 exports.checkBodyData = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -30,45 +26,34 @@ exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours
-    }
+    requestedAt: req.requestTime
+    // results: tours.length,
+    // data: {
+    //   tours
+    // }
   });
 };
 
 // GET Single Tour
 exports.getTour = (req, res) => {
   const id = req.params.id * 1; // converting number
-  const requestedTour = tours.find(tour => tour.id === id);
+  // const requestedTour = tours.find(tour => tour.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      requestedTour
-    }
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     requestedTour
+  //   }
+  // });
 };
 // POST
 exports.createTour = (req, res) => {
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  console.log(tours);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success'
+    // data: {
+    //   tour: newTour
+    // }
+  });
 };
 // PATCH
 exports.updateTour = (req, res) => {
@@ -85,3 +70,8 @@ exports.deleteTour = (req, res) => {
     data: null
   });
 };
+
+// testing purpose, should be on the top
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
