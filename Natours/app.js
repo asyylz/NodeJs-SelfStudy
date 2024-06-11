56b6b12cd61b64b678753b40804be3e92250fcdf
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-11 21:26:41 +0100

    error handler and ndb

--- Natours/app.js
+++ Natours/app.js
@@ -29,10 +29,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter); // mounting router
 app.use('/api/v1/tours', tourRouter); // mounting router
 
-//app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 module.exports = app;
