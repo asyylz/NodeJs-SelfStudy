372b7a46e524e16b60d883dbd2f7410516122b4b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-12 23:54:36 +0100

    "Refactor errorController.js: updated AppError import path, removed console logs, and rearranged error handling logic"

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require('../utils/appError');
+const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
@@ -12,17 +12,14 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
-
 const handleValidationErrorDB = err => {
-  console.log('asiye1');
   const errors = Object.values(err.errors).map(el => el.message);
-  console.log(errors);
+
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
 const sendErrorDev = (err, res) => {
-  console.log('asiyedev');
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -57,19 +54,17 @@ module.exports = (err, req, res, next) => {
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  if (process.env.NODE_ENV === 'development') {
-    if (err.name === 'ValidationError') handleValidationErrorDB(err);
 
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
-    console.log('asiye', error.name);
 
     if (error.name === 'CastError' || error.path === '_id')
-      // name field is not available in error objeect
       error = handleCastErrorDB(error);
-    if (err.errorResponse.code === 11000)
-      error = handleDuplicateFieldsDB(err.errorResponse);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
